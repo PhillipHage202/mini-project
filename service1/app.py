db3659ae8d892732c65238185aa91cae4dc3ab4f
unknown <you@example.com>
2020-11-24 16:27:55 +0000

    dockerfile

--- service1/app.py
+++ service1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,  request
 
 import requests
 
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route('/', methods =['GET'])
 def index():
     # get animal
-    animal = requests.get("http://localhost:5001/animal")
+    animal = requests.get("34.105.211.246:5001/animal")
     # get noise
-    noise = requests.post("http://localhost:5001/noise", data=animal.text)
+    noise = requests.post("34.105.211.246:5001/noise", data=animal.text)
 
     return render_template ('index.html', animal=animal.text, noise=noise.text )
 
 if __name__ == '__main__':
-    app.run( debug=True, host='0.0.0.0')
+    app.run( debug=True, host='0.0.0.0', port=5000)

39722fcf4d86346924063af6258e6a510d752419
peehage <peehage202@gmail.com>
2020-12-03 19:41:10 +0000

    it works

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -5,7 +5,7 @@ import requests
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('index.html', title='Home')
 
 
 @app.route('/get_char', methods=['GET'])

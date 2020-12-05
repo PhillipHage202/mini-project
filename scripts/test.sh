fee6478b88da034e96e54aed4a995e10041544c8
peehage <peehage202@gmail.com>
2020-12-05 13:00:26 +0000

    updated

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+docker build -f testing/Dockerfile -t testing-image .
+docker run -it -d --name testing-container testing-image
 
 docker exec testing-container pytest ./service1 --cov ./service1
 docker exec testing-container pytest ./service2 --cov ./service2

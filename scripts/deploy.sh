3d8167ac519fe41e0029f57e4ec91353be137dd9
peehage <peehage202@gmail.com>
2020-12-05 23:43:19 +0000

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
-ssh peehage202@34.89.4.21 <<EOF
+ssh swarm-manager  <<-EOF
+
+docker pull phage2022/service1:latest
+docker pull phage2022/service2:latest
+docker pull phage2022/service3:latest
+docker pull phage2022/service4:latest
+
+
+git clone https://github.com/PhillipHage202/practical-project.git
 
 cd practical-project
 
-git pull
 
-docker stack deploy --compose-file docker-compose.yaml practical-project#
+
+docker stack deploy --compose-file docker-compose.yaml practical-project
 
 EOF 

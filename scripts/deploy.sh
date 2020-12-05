62ab33d4d4e4ac1284b9a382b8b2a8e7459bb886
peehage <peehage202@gmail.com>
2020-12-05 21:46:05 +0000

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
-ssh peehage202@34.89.4.21 << EOF
+
+ssh peehage202@34.89.4.21 <<EOF
 
 cd practical-project
+
 git pull
-docker stack deploy --compose-file docker-compose.yaml practical-project
+
+docker stack deploy --compose-file docker-compose.yaml practical-project#
 
 EOF 

e724a9fa84c42895c6b0d4626731362742995fda
peehage <peehage202@gmail.com>
2020-12-05 13:38:33 +0000

    updated

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+export DB_URI: ${DATABASE_URI}
+export KEY: ${MY_SECRET_KEY}
 
 docker build -f testing/Dockerfile -t testing-image .
 docker run -it -d --name testing-container testing-image
 
-docker exec testing-container pytest ./service1 --cov ./service1
-docker exec testing-container pytest ./service2 --cov ./service2
-docker exec testing-container pytest ./service3 --cov ./service3
-docker exec testing-container pytest ./service4 --cov ./service4
+sudo docker exec testing-container pytest ./service1 --cov ./service1
+sudo docker exec testing-container pytest ./service2 --cov ./service2
+sudo docker exec testing-container pytest ./service3 --cov ./service3
+sudo docker exec testing-container pytest ./service4 --cov ./service4
 
 docker stop testing-container
 docker rm testing-container

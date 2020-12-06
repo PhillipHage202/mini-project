92cccd02258ad1165fb2443b1c691baea65d2960
root <root@hehehe.europe-west2-a.c.come-on-295911.internal>
2020-12-06 02:12:55 +0000

    updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
+
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+. ~/.bashrc
+pip install --user ansible
+echo $PATH
+ansible-playbook -i ./inventory ./playbook.yaml
+
+sudo chmod 666 /var/run/docker.sock
 docker --version
 docker-compose down --rmi all
 docker-compose pull

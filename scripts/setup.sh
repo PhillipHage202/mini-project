a36756a540bbd74e838aa15da513cd104fec3ab7
unknown <you@example.com>
2020-12-06 22:34:06 +0000

    up

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/PhillipHage202/practical-project.git
 cd practical-project
 echo $PATH
-sudo /home/jenkins/.local/bin/ansible-playbook -i inventory playbook.yaml
+sudo ansible-playbook -i inventory playbook.yaml
